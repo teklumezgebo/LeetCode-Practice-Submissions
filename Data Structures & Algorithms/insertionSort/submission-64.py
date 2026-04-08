8a6cd1bb4dcee62e6657c109adf468ea060102df
Teklu Mezgebo <113315510+teklumezgebo@users.noreply.github.com>
2026-04-08 12:25:04 -0500

    Add: insertionSort - submission-64

---
+++ Data Structures & Algorithms/insertionSort/submission-64.py
@@ -0,0 +1,23 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def insertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        variations = []
+
+        if len(pairs) == 0:
+            return []
+
+        for i in range(len(pairs)):
+            j = i - 1
+            while j >= 0 and pairs[j].key > pairs[j + 1].key:
+                tmp = pairs[j + 1]
+                pairs[j + 1] = pairs[j]
+                pairs[j] = tmp
+                j -= 1
+
+            variations.append(pairs[:])
+
+        return variations
