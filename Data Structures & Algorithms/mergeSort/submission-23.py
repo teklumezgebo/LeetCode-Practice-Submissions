3cbd94f7f0163929c33ef1ef23c29706c54a318b
Teklu Mezgebo <113315510+teklumezgebo@users.noreply.github.com>
2026-04-08 19:21:58 -0500

    Add: mergeSort - submission-23

---
+++ Data Structures & Algorithms/mergeSort/submission-23.py
@@ -0,0 +1,51 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def merge(self, arr, s, m, e):
+        L = arr[s:m + 1]
+        R = arr[m + 1:e + 1]
+
+        i = 0
+        j = 0
+        k = s
+
+        while i < len(L) and j < len(R):
+            if L[i].key <= R[j].key: 
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            
+            k += 1
+            
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1 
+        
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+    def mergeSort(self, arr: List[Pair], s=None, e=None) -> List[Pair]:
+        if s is None:
+            s = 0
+        if e is None:
+            e = len(arr) - 1
+            
+        if (e - s) + 1 <= 1:
+            return arr
+
+        m = (s + e) // 2
+
+        self.mergeSort(arr, s, m)
+        self.mergeSort(arr, m + 1, e)
+        
+        self.merge(arr, s, m, e)
+
+        return arr
