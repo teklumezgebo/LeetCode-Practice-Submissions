7af7048999113efdbf3a890b05aef0636566b1a6
Teklu Mezgebo <113315510+teklumezgebo@users.noreply.github.com>
2026-04-10 14:58:05 -0500

    Add: quickSort - submission-23

---
+++ Data Structures & Algorithms/quickSort/submission-23.py
@@ -0,0 +1,31 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def quickSort(self, pairs: List[Pair], s=None, e=None) -> List[Pair]:
+        if s is None:
+            s, e = 0, len(pairs) - 1
+
+        if e - s + 1 <= 1:
+            return pairs
+
+        pivot = pairs[e]
+
+        left = s
+
+        for i in range(s, e):
+            if pairs[i].key < pivot.key:
+                tmp = pairs[left]
+                pairs[left] = pairs[i]
+                pairs[i] = tmp
+                left += 1
+
+        pairs[e] = pairs[left]
+        pairs[left] = pivot
+
+        self.quickSort(pairs, s, left - 1)
+        self.quickSort(pairs, left + 1,  e)
+
+        return pairs
